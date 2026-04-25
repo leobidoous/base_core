457c42fce096201dd8b2109b47839aa791f0f02e
leobidoous <leobidoous@icloud.com>
2026-04-25 03:51:37 -0300

    feat: add toNumber function

--- lib/src/core/utils/formats/number_formats.dart
+++ lib/src/core/utils/formats/number_formats.dart
@@ -23,4 +23,23 @@ class NumberFormat {
       return format(0).trim();
     }
   }
+
+  static String toNumber(
+    dynamic value, {
+    String? locale = 'pt_BR',
+    int? decimalDigits = 0,
+  }) {
+    final format = nf.NumberFormat.decimalPatternDigits(
+      locale: locale,
+      decimalDigits: decimalDigits ?? 0,
+    ).format;
+    try {
+      if (value.toString().contains('e')) {
+        return format(value).trim();
+      }
+      return format(value.toString().tryParseCurrencyToDouble).trim();
+    } catch (e) {
+      return format(0).trim();
+    }
+  }
 }
